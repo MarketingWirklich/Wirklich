856120ef196c66c85af8a0a15390842604b113c4
Wesley Carvalho <crafael.wesley@gmail.com>
2022-11-30 11:01:55 -0300

    fix

--- src/components/BlogBanner.tsx
+++ src/components/BlogBanner.tsx
@@ -8,7 +8,7 @@ export function BlogBanner({image}: IBlogBanner) {
     return (
         <>
         <Box className="w-full relative">
-        <img className="w-full h-[570px]" src={image} />
+        <img className="w-full h-[300px] lg:h-[570px] 2xl:h-[680px]" src={image} />
         </Box>
         </>
     )
